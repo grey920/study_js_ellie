d66f79de6044f6bb9d9d6b8b141c6df6d541cb6a
grey920 <gyuwoonj@gmail.com>
2021-01-12 20:37:34 +0900

    변수 let

--- main.js
+++
@@ -1,9 +0,0 @@
-// 순수 바닐라 자바스크립트를 쓸 때에는 use strict를 선언한다
-// 이유: js는 아주 유연하기 때문에 선언되지 않은 변수에 값을 할당하는 등 
-//      비상식적인 행동이 가능하기 때문에 이를 막아줄 수 있는 'use strict'를 선언한다
- 'use strict';
-
-console.log('hello world');
-
-let a; // let a;를 주석처리하면 'a is not defined'라는 에러 메시지 발생
-a = 6;
---
+++ variable.js
@@ -0,0 +1,13 @@
+// 1. Use strict
+// added in ES 5
+// use this for Vanilla Javascript
+ 'use strict';
+
+// 2. Variable (변수)
+// let (added in ES6)
+
+// 선언과 동시에 값 할당
+let name = 'grey'; // name이라는 변수가 가리키는 메모리 어딘가에 grey라는 값을 저장함.
+console.log(name);
+name = 'hello'; // 값 다시 할당
+console.log(name);
